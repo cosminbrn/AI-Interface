4dcd0a590b833c424ce83daf3e82778a23dbdd99
cosminbrn <cosminbaroana06@gmail.com>
2026-03-18 13:32:14 +0200

    Remove redundant content

--- frontend/src/components/modelMessageContainer/ModelTextArea/ModelTextArea.tsx
+++ frontend/src/components/modelMessageContainer/ModelTextArea/ModelTextArea.tsx
@@ -6,27 +6,7 @@ export default function ModelTextArea() {
             <p>The vw unit represents 1% of the viewport's width, while vh represents 1% of the viewport's height. Vmin is equal to the smaller of vw or vh, and vmax is equal to the larger of vw or vh.16 feb. 2024
 CSS Viewport Units: vh, vw, vmin, and vmax - SitePoint
 
-SitePoint
-https://www.sitepoint.com › css-viewport-units-quick-start
-Despre fragmentele recomandate
-•
-Feedback
-Explain the concept of the 'vh' and 'vw' units in CSS
-
-GeeksforGeeks
-https://www.geeksforgeeks.org › ...
-·
-Traducerea acestei pagini
-18 iun. 2024 — The vh (viewport height) and vw (viewport width) units in CSS are relative length units representing a percentage of the viewport dimensions ...
-CSS Units
-
 W3Schools
-https://www.w3schools.com › ...
-·
-Traducerea acestei pagini
-Relative Lengths ; vw, Relative to 1% of the width of the viewport*, Try it ; vh, Relative to 1% of the height of the viewport*, Try it ; vmin, Relative to 1% of ...
-Oamenii au mai întrebat și
-What is the difference between 100% and 100vh in CSS?
 How much is 1 vh?
 What is vh and vm?
 What is VW in width?
