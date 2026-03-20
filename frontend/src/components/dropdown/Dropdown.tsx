447e5576570587a3cf6272df26a67c5e3949ca42
cosminbrn <cosminbaroana06@gmail.com>
2026-03-20 21:10:49 +0200

    Add Dropdown button dropdown menu

--- frontend/src/components/dropdown/Dropdown.tsx
+++ frontend/src/components/dropdown/Dropdown.tsx
@@ -1,11 +1,32 @@
 import styles from './Dropdown.module.scss';
 import arrow from '../../assets/arrow.svg'
+import { useState } from 'react';
 
 export default function Dropdown() {
+    const [isOpen, setOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState('GPT-4o');
+
+    const toggleDropdown = () => {
+        setOpen(!isOpen);
+    }
+
+    const handleOptionClick = (option: string) => {
+        setSelectedOption(option);
+        setOpen(false);
+    }
+
     return (
         <div className={styles.dropdown}>
-            <div className={styles.text}>GPT-4o</div>
-            <img src={arrow} alt="Dropdown Arrow" className={styles.arrow} />
+                <div className={styles.text} onClick={toggleDropdown}>
+                    {selectedOption}
+                </div>
+                <img src={arrow} alt="Dropdown Arrow" className={styles.arrow} onClick={toggleDropdown} />
+            {isOpen && (
+                <div className={styles.menu}>
+                    <div className={styles.menuItem} onClick={() => handleOptionClick('GPT-4o')}>GPT-4o</div>
+                    <div className={styles.menuItem} onClick={() => handleOptionClick('GPT-3.5')}>GPT-3.5</div>
+                </div>
+            )}
         </div>
     )
 }
