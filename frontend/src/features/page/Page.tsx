aa264fd83008004032939690486369f682df8f7b
cosminbrn <cosminbaroana06@gmail.com>
2026-03-21 07:53:55 +0200

    Add input component to page

--- frontend/src/features/page/Page.tsx
+++ frontend/src/features/page/Page.tsx
@@ -1,12 +1,14 @@
 import { Header } from "../../layouts/header/Header"
 import Chat from "../../layouts/chat/Chat"
 import styles from './Page.module.scss';
+import Input from "../../components/Input/Input";
 
 export default function Page() {
     return (
         <div className={styles.page}>
             <Header />
             <Chat />
+            <Input />
         </div>
     )
 }
--- frontend/src/layouts/chat/Chat.tsx
+++ frontend/src/layouts/chat/Chat.tsx
@@ -1,7 +1,6 @@
 import styles from './Chat.module.scss';
 import ModelMessageContainer from '../../components/modelMessageContainer/ModelMessageContainer';
 import UserMessageContainer from '../../components/userMessageContainer/UserMessageContainer';
-import Input from '../../components/Input/Input';
 
 export default function Chat() {
     return (
