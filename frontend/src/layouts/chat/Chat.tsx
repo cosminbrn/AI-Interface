89086050cb5fb827aaf287f402d97447a36b117d
cosminbrn <cosminbaroana06@gmail.com>
2026-03-21 07:47:22 +0200

    Update Chat container to support multiple prompts and replies

--- frontend/src/layouts/chat/Chat.tsx
+++ frontend/src/layouts/chat/Chat.tsx
@@ -1,10 +1,13 @@
 import styles from './Chat.module.scss';
 import ModelMessageContainer from '../../components/modelMessageContainer/ModelMessageContainer';
 import UserMessageContainer from '../../components/userMessageContainer/UserMessageContainer';
+import Input from '../../components/Input/Input';
 
 export default function Chat() {
     return (
         <div className={styles.chat}>
+            <ModelMessageContainer />
+            <UserMessageContainer />
             <ModelMessageContainer />
             <UserMessageContainer />
         </div>
