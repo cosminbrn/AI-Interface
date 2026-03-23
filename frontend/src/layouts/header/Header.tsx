18fc57a0e03468eaee3bd2feda94b6f954305ca3
cosminbrn <cosminbaroana06@gmail.com>
2026-03-23 13:50:27 +0200

    Replace AI version change dropdown with session title

--- frontend/src/layouts/header/Header.tsx
+++ frontend/src/layouts/header/Header.tsx
@@ -1,19 +1,21 @@
 import styles from './Header.module.scss'
-import Dropdown from '../../components/dropdown/Dropdown'
 import burgerIcon from '../../assets/burger.svg';
 import shareIcon from '../../assets/share.svg';
 
 interface HeaderProps {
     onMenuClick: () => void;
+    currentSession: string;
 }
 
-export function Header({ onMenuClick }: HeaderProps) {
+export function Header({ onMenuClick, currentSession }: HeaderProps) {
     return (
         <nav className={styles.navbar}>
             <div className={styles.burger} onClick={onMenuClick}>
                 <img src={burgerIcon} alt="Menu" />
             </div>
-            <Dropdown />
+            <div className={styles.sessionName}>
+                {currentSession}
+            </div>
             <div className={styles.share}>
                 <img src={shareIcon} alt="Share" />
             </div>
